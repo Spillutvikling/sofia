d177fe8d74ca1b1a63a69e81ef7bd171046e1f0d
Bjørn Tore Gjerde <bjorntore.gjerde@bouvet.no>
2018-06-04 23:20:20 +0200

    Add demo of visual part of spectral vision. #9

---
+++ Sofia/Assets/Music/MusicPlayer.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public List<AudioClip> songs;
+    private float originalVolume;
+
+    private int currentSong = 0;
+    private bool isFadingOut = false;
+
+    private void Start()
+    {
+        if (!Settings.Sound_EnableMusic)
+        {
+            audioSource.enabled = false;
+            enabled = false;
+            return;
+        }
+
+        songs.Shuffle();
+        originalVolume = audioSource.volume;
+    }
+
+    private void Update()
+    {
+        if (!SpectralMode.instance.spectralEnabled && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+            return;
+        }
+
+        if (!audioSource.isPlaying)
+        {
+            currentSong++;
+            if (currentSong >= songs.Count)
+                currentSong = 0;
+
+            audioSource.clip = songs[currentSong];
+            StartCoroutine(FadeIn(audioSource, 3f));
+        }
+        else
+        if (!isFadingOut && (audioSource.clip.length - audioSource.time < 5))
+            StartCoroutine(FadeOut(audioSource, 5f));
+    }
+
+    public IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+    {
+        isFadingOut = true;
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+            yield return null;
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+        isFadingOut = false;
+
+        yield return null;
+    }
+
+    public IEnumerator FadeIn(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = 0.2f;
+        audioSource.volume = 0;
+        audioSource.Play();
+
+        while (audioSource.volume < originalVolume)
+        {
+            audioSource.volume += startVolume * Time.deltaTime / FadeTime;
+            yield return null;
+        }
+
+        audioSource.volume = originalVolume;
+
+        yield return null;
+    }
+}
---
+++ Sofia/Assets/Visual Effects/SpectralMode.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SpectralMode : MonoBehaviour
+{
+    public PostProcessProfile spectralEffect;
+
+    PostProcessVolume m_Volume;
+    PostProcessProfile originalProfile;
+    Vignette m_Vignette;
+    public bool spectralEnabled = false;
+
+    public static SpectralMode instance;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+        {
+            Debug.LogError("Tried to create another instance of " + GetType() + ". Destroying.");
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        m_Volume = GetComponent<PostProcessVolume>();
+        originalProfile = m_Volume.profile;
+    }
+
+    void Update()
+    {
+        HandleSpectralToggle();
+        AnimateVignette();
+    }
+
+    private void HandleSpectralToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (spectralEnabled)
+            {
+                m_Volume.profile = originalProfile;
+                spectralEnabled = false;
+            }
+            else
+            {
+                m_Volume.profile = spectralEffect;
+                m_Volume.profile.TryGetSettings(out m_Vignette);
+                spectralEnabled = true;
+            }
+        }
+    }
+
+    private void AnimateVignette()
+    {
+        if (spectralEnabled)
+        {
+            var value = (Mathf.Sin(0.7f * Time.realtimeSinceStartup) * 0.04f) + 0.32f;
+            m_Vignette.intensity.Override(value);
+        }
+    }
+}
