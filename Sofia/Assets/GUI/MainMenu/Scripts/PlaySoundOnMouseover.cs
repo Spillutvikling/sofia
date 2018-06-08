5b4be4703948b2546db7df9aa4b65e4325e477fc
Bjørn Tore Gjerde <bjorntore.gjerde@bouvet.no>
2018-06-08 22:41:19 +0200

    Add clicking sound to main menu (for fun)

---
+++ Sofia/Assets/GUI/MainMenu/Scripts/PlaySoundOnMouseover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlaySoundOnMouseover : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioSource audioSource;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        audioSource.Play();
+    }
+}
