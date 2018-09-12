3c26555c7c564999d8bfb113e1ecd70e15d8c56d
Bjørn Tore Gjerde <bjorntore.gjerde@bouvet.no>
2018-09-12 21:25:20 +0200

    Split rooms into separate scenes anyway. Change scene by trigger.

---
+++ Sofia/Assets/GUI/UI/Scripts/SceneNameLabel.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNameLabel : MonoBehaviour
+{
+
+    public TextMeshProUGUI text;
+
+    void Start()
+    {
+        text.text = SceneManager.GetActiveScene().name;
+    }
+}
---
+++ Sofia/Assets/Game Logic/SceneLoadTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadTrigger : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var playerController = other.GetComponent<PlayerController>();
+
+        if (playerController != null)
+        {
+            var nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            var totalNumberOfScenes = SceneManager.sceneCountInBuildSettings;
+
+            if (nextIndex >= totalNumberOfScenes)
+            {
+                Debug.LogError($"Tried to load scene index {nextIndex}, but there are only {totalNumberOfScenes} scenes in build config.");
+                return;
+            }
+
+            SceneManager.LoadScene(nextIndex);
+        }
+    }
+}
