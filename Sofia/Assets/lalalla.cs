a431939ace80916019f35abd0125f9ef85e46c7d
Axel Sanner <axel.sanner@bisnode.com>
2018-10-30 21:19:33 +0100

    Slett meg

---
+++ Sofia/Assets/lalalla.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lalalla : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dx_door = 1.0f;
+        float delta = Time.deltaTime;
+        GameObject axel = gameObject;
+        float speed = dx_door * delta;
+
+        bool accel_x = true;
+        axel.transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+	}
+}
